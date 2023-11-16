007fb60ed971026a3c3de9d6fc03f703cd446198
Rodrigo <rodrigoncr99@gmail.com>
2023-11-16 17:32:23 -0600

    Second commit

--- src/Sections/Portada/Portada.jsx
+++ src/Sections/Portada/Portada.jsx
@@ -5,7 +5,7 @@ import Carousel from '../../Components/Carousel/Carousel.jsx'
 export default function Portada() {
   return (
     <>
-        <h1>Aqui va la portada</h1>
+        <h1>Esta es la portada </h1>
         <Carousel></Carousel>
 
     </>
